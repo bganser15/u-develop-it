af09300ae037bba2aa0ad554fc3a599c51604bc3
Brittany Ganser <15brganser@gmail.com>
2022-04-22 19:30:39 -0600

    retrieved data from candidate table through query

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
+
+require("dotenv").config();
+const password = process.env.MYPASSWORD;
+//console.log(process.env); // remove this after you've confirmed it working
 
 //middleware
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to sql database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: password,
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
 
 //posts hello world in localhost:3001
 app.get("/", (req, res) => {
@@ -14,6 +30,9 @@ app.get("/", (req, res) => {
   });
 });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 //default response for any other request that is not found
 app.use((req, res) => {
   res.status(404).end();
