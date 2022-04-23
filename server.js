24403f34ea1df9580252eb46f9e9df6eee07f3e3
Brittany Ganser <15brganser@gmail.com>
2022-04-22 20:48:29 -0600

    added CRUD routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require("mysql2");
+const inputCheck = require("./utils/inputCheck");
 
 require("dotenv").config();
 const password = process.env.MYPASSWORD;
@@ -64,6 +65,56 @@ app.get("/api/candidate/:id", (req, res) => {
   });
 });
 
+// Delete a candidate
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Candidate not found",
+      });
+    } else {
+      res.json({
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
+
+// Create a candidate
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
 // Create a candidate
 //const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
 //VALUES (?,?,?,?)`;
@@ -97,5 +148,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on port ${PORT}");
+  console.log(`Server running on port ${PORT}`);
 });
